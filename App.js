2250e6d57564e5475bf70dc10a762f1e3cf42ac0
curioskills <curioskills@gmail.com>
2024-03-14 15:38:24 +0530

    timed interval component

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { ProgressBar } from "react-native-paper";
+import { useState, useEffect, useRef } from "react";
 
 export default function App() {
+  const [isActive, setIsActive] = useState(true);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    let timer;
+    isActive
+      ? (timer = setInterval(() => {
+          console.log("hello world");
+          setProgress((progress) => progress + 0.1);
+        }, 1000))
+      : clearInterval(timer);
+    return () => clearInterval(timer);
+  }, [isActive]);
+
+  useEffect(() => {
+    if (progress >= 1) {
+      setIsActive(false);
+    }
+  }, [progress]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {progress>=1?<Text>This text is displayed only after 10 seconds</Text>:null}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
