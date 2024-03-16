139ef0cdc37b0fac3042394d12bd3fa68fcbf6be
curioskills <curioskills@gmail.com>
2024-03-16 08:38:04 +0530

    Error screen UI

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={require("./assets/images/404.png")}
+        style={{ width: Dimensions.get("screen").width - 80, height: 320 }}
+      />
+      <Text
+        style={{ fontWeight: "bold", fontSize: 25, textAlign: "center" }}
+      >{`Oops! You weren't \n supposed to see this`}</Text>
+      <Text style={{ fontWeight: "600", fontSize: 12, marginVertical: 10 }}>
+        The page you were looking was loading incorrectly
+      </Text>
+      <TouchableOpacity
+        onPress={() => {}}
+        style={{
+          backgroundColor: "#000",
+          paddingVertical: 5,
+          paddingHorizontal: 20,
+          borderRadius: 10,
+        }}
+      >
+        <Text
+          style={{
+            color: "#fff",
+            fontWeight: "600",
+            fontSize: 14,
+            marginVertical: 10,
+          }}
+        >
+          Go Back
+        </Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +49,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fcb82f",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
